3601f78d86fb4438036da369cff959d03f2683ce
jmhill91 <john.m.hill91@gmail.com>
2020-04-16 02:25:07 -0400

    update for cart relationships

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
 
 	belongs_to :business
+	has_many :carts 
+	has_many :products, through: :carts
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
 
 	has_many :appointments
+	has_many :carts 
+	has_many :products, through: :carts
 end

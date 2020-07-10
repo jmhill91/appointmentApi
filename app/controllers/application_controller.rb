751cfdf223fbe652f1ea93add478380cc96011e2
jmhill91 <john.m.hill91@gmail.com>
2020-07-09 22:19:28 -0700

    messing with auth  for seperate type of users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  before_action :authorized
 
   def encode_token(payload)
     JWT.encode payload, secret, 'HS256'
@@ -14,6 +15,22 @@ class ApplicationController < ActionController::API
 
   def decoded_token
     JWT.decode token, secret, true, { algorithm: 'HS256' }
+  end
+  
+  def user_payload(user)
+    { user_id: user.id }
+  end
+
+  def current_user
+    User.find(decoded_token[0]["user_id"])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    render json: { message: "Please log in to continue"}, status: :unauthorized unless logged_in?
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,6 @@ class UsersController < ApplicationController
 
 	private 
 
-	def user_payload(user)
-    	{ user_id: user.id }
-  	end
 
-  	def current_user
-    	User.find(decoded_token[0]["user_id"])
-  	end
 
 end
